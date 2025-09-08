dc52e0ff0e34fae511afffa8a1b3affbb1939ce3
Mark Simon <msimonbringas05v2@gmail.com>
2025-09-08 21:55:00 +0800

    Add user promptloop

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 import ollama
 
 model_name = 'ooga-booga:latest'
-question = input("Give words. Ooga-booga.\n>>> ")
 
-try:
+while True:
+    question = input("Give words. Ooga-booga.\n>>> ")
+    
+    if question.strip().lower() in ['exit', 'quit']:
+        print("Stop. Ooga-booga.")
+        break
+
     # prompt to your heart's content 
-    stream = ollama.chat(
-        model=model_name,
-        messages=[{'role': 'user', 'content': question}],
-        stream = True
-    )
+    try:
+        stream = ollama.chat(
+            model=model_name,
+            messages=[{'role': 'user', 'content': question}],
+            stream = True
+        )
 
-    for chunk in stream:
-        print(chunk['message']['content'], end='', flush=True)
+        for chunk in stream:
+            print(chunk['message']['content'], end='', flush=True)
+        
+        print("\n")
 
-except ollama.ResponseError as err:
-    # if model has not been downloaded yet
-    if err.status_code == 404:
-        print(f"Model '{model_name}' not found. Pulling model...")
-        ollama.pull(model_name)
-        print(f"Model '{model_name}' pulled successfully. Please re-run the script.")
-    else :
-        print(f"Error occurred: {err.error}")
+    except ollama.ResponseError as err:
+        # if model has not been downloaded yet
+        if err.status_code == 404:
+            print(f"Model '{model_name}' not found. Pulling model...")
+            ollama.pull(model_name)
+            print(f"Model '{model_name}' pulled successfully. Please re-run the script.")
+        else :
+            print(f"Error occurred: {err.error}")
